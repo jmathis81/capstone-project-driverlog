4d5558b3d46efd2b647b3a4dadeb2834ad516413
Jesse Mathis <jmathis7@radar.gsw.edu>
2026-02-18 22:45:46 -0500

    Updated index.js file to include new me function

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ require("./src/functions/startRoute");
 require("./src/functions/endRoute");
 require("./src/functions/uploadPoints");
 require("./src/functions/getSummaries");
+require("./src/functions/me");
 
 // Set up the Functions app
 app.setup({ enableHttpStream: true });
