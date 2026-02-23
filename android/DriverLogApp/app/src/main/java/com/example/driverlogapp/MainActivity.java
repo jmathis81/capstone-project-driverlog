7104b6e06c8b49097151118c19993c49336fdfde
StudentWorker835 <ajorda14@radar.gsw.edu>
2026-02-23 08:35:00 -0500

    Hopefully resolved dependency mess caused by the activity switch and started testing implementation of authentication

--- android/DriverLogApp/app/src/main/java/com/example/driverlogapp/MainActivity.java
+++ android/DriverLogApp/app/src/main/java/com/example/driverlogapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -15,6 +16,15 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.*;
 import com.google.android.gms.location.*;
+import com.microsoft.identity.client.IAccount;
+import com.microsoft.identity.client.AuthenticationCallback;
+import com.microsoft.identity.client.IPublicClientApplication;
+import com.microsoft.identity.client.ISingleAccountPublicClientApplication;
+import com.microsoft.identity.client.IAuthenticationResult;
+import com.microsoft.identity.client.PublicClientApplication;
+import com.microsoft.identity.client.SilentAuthenticationCallback;
+import com.microsoft.identity.client.exception.MsalException;
+import com.microsoft.identity.common.java.request.ILocalAuthenticationCallback;
 
 
 import java.io.IOException;
@@ -23,16 +33,99 @@ import okhttp3.*;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ISingleAccountPublicClientApplication mSingleAccountApp;
+    private String accessToken;
+    private static final String[] scopes = {"api://8a653568-903e-4249-aa36-373da6f46ffa/access_as_user"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeMsal();
+    }
+    private void initializeMsal() {
+        PublicClientApplication.createSingleAccountPublicClientApplication(MainActivity.this, R.raw.auth_config, new IPublicClientApplication.ISingleAccountApplicationCreatedListener() {
+            @Override
+            public void onCreated(ISingleAccountPublicClientApplication application) {
+                mSingleAccountApp = application;
+                loadAccount();
+            }
 
-
+            @Override
+            public void onError(MsalException exception) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private void loadAccount() {
+        if (mSingleAccountApp == null) return;
+
+        mSingleAccountApp.getCurrentAccountAsync(new ISingleAccountPublicClientApplication.CurrentAccountCallback() {
+            @Override
+            public void onAccountLoaded(@Nullable IAccount account) {
+                if (account != null) {
+                    getAccessToken(account);
+                } else {
+                    signIn();
+                }
+            }
+
+            @Override
+            public void onAccountChanged(@Nullable IAccount priorAccount, @Nullable IAccount currentAccount) {
+                if (currentAccount == null) {
+                    signIn();
+                }
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void signIn() {
+        if (mSingleAccountApp == null) return;
+
+        mSingleAccountApp.signIn(MainActivity.this, null, scopes, new AuthenticationCallback() {
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                accessToken = authenticationResult.getAccessToken();
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getAccessToken(IAccount account) {
+        if (mSingleAccountApp == null) return;
+
+        mSingleAccountApp.acquireTokenSilentAsync(scopes, account.getTenantId(), new SilentAuthenticationCallback() {
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                accessToken = authenticationResult.getAccessToken();
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                signIn();
+            }
+
+        });
+    }
+
+    public String getAuthorizationHeader() {
+        return "Bearer " + accessToken;
+    }
 
 }
